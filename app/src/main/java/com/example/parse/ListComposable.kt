69793f6f3910f5d5a93f332e03fc9efbd4b38499
vvempati93 <vasavivempati2@gmail.com>
2023-02-22 10:53:24 -0800

    initial

---
+++ app/src/main/java/com/example/parse/ListComposable.kt
@@ -0,0 +1,11 @@
+package com.example.parse
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ListComposable() {
+    LazyColumn {
+
+    }
+}
